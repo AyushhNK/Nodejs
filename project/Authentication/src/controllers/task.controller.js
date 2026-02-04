0325419f9314a9db66eec2da85097d5e5d4f232b
AyushhNK <neupaneayush3@gmail.com>
2026-02-04 18:56:13 +0545

    feat(task): update to task controller

--- project/Authentication/src/controllers/task.controller.js
+++ project/Authentication/src/controllers/task.controller.js
@@ -26,3 +26,36 @@ export const getTasks = async (req, res) => {
     res.status(500).json({ message: error.message });
   }
 };
+
+export const DeleteTask = async (req, res) => {
+  try {
+    const userId = req.user.userId;
+    const taskId = req.params.id;
+    const task = await TaskModel.findOneAndDelete({ _id: taskId, userId });
+    if (!task) {
+      return res.status(404).json({ message: "Task not found" });
+    }
+    res.status(200).json({ message: "Task deleted successfully" });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+export const UpdateTask = async (req, res) => {
+  try {
+    const userId = req.user.userId;
+    const taskId = req.params.id; 
+    const { title, description, status } = req.body;
+    const task = await TaskModel.findOneAndUpdate(
+      { _id: taskId, userId },
+      { title, description, status },
+      { new: true }
+    );
+    if (!task) {
+      return res.status(404).json({ message: "Task not found" });
+    }
+    res.status(200).json(task);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  } 
+};
